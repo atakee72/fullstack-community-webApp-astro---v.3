fcd024074a407c432cbc37a511f5278d4ee0fb6a
atakee72 <atakee@gmail.com>
2025-10-23 22:55:00 +0200

    Complete migration to root structure and fix prettier config

--- server/routes/announcementRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllAnnouncements } from "../controller/announcementController.js";
-
-const router = express.Router();
-
-router.get("/all", getAllAnnouncements);
-
-export default router;
--- server/routes/recommendationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllRecommendations } from "../controller/recommendationController.js";
-
-const router = express.Router();
-
-router.get("/all", getAllRecommendations);
-
-export default router;
