752fbf4eabc55e5a5979ccc39e2085e6baff1589
Ercan Atak <atakee@gmail.com>
2026-04-17 20:11:59 +0200

    fix: ensure magnifying glass icon renders in marketplace search (wrap in flex div with z-10)

--- src/components/marketplace/SearchBar.svelte
+++ src/components/marketplace/SearchBar.svelte
@@ -19,14 +19,11 @@
 
 <form onsubmit={handleSubmit} class="relative w-full">
   <div class="relative">
-    <svg
-      class="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-white/50"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-    </svg>
+    <div class="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none z-10">
+      <svg class="w-5 h-5 text-white/50" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </div>
     <input
       type="text"
       value={query}
